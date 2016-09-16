e88bc8781dd6b5aa1a1d038d156a26ba627dc14a
Mikhail Putilov <post.snowy@gmail.com>
2016-09-16 14:48:46 +0300

    Add shell sort

--- src/main/java/ru/innopolis/mputilov/Collections.java
+++ src/main/java/ru/innopolis/mputilov/Collections.java
@@ -1,5 +1,6 @@
 package ru.innopolis.mputilov;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Collections.swap;
@@ -8,6 +9,8 @@ import static java.util.Collections.swap;
  * Created by mputilov on 16.09.16.
  */
 public class Collections {
+    private static final List<Integer> SHELL_SORT_JUMP_CONSTANTS = Arrays.asList(40, 13, 4, 1);
+
     public static <T extends Comparable<T>> void insertionSort(List<T> list) {
         if (list.size() <= 1) {
             return;
@@ -20,6 +23,26 @@ public class Collections {
                     swap(list, current + 1, current);
                 } else {
                     break;
+                }
+            }
+        }
+    }
+
+    public static <T extends Comparable<T>> void shellSort(List<T> list) {
+        if (list.size() <= 1) {
+            return;
+        }
+        for (Integer jump : SHELL_SORT_JUMP_CONSTANTS) {
+            for (int firstUnsortedFromLeft = 1; firstUnsortedFromLeft < list.size(); firstUnsortedFromLeft += jump) {
+                T insertThis = list.get(firstUnsortedFromLeft);
+                //find place where to insert:
+                for (int current = firstUnsortedFromLeft - jump; current >= 0; current -= jump) {
+                    T maybeGoRight = list.get(current);
+                    if (insertThis.compareTo(maybeGoRight) < 0) {
+                        swap(list, current + jump, current);
+                    } else {
+                        break;
+                    }
                 }
             }
         }
--- src/test/java/ru/innopolis/mputilov/CollectionsTest.java
+++ src/test/java/ru/innopolis/mputilov/CollectionsTest.java
@@ -1,5 +1,7 @@
 package ru.innopolis.mputilov;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +14,7 @@ public class CollectionsTest {
 
     private List<Integer> testList = new ArrayList<>();
 
-    @org.junit.Test
+    @Test
     public void insertionSort1() throws Exception {
         testList.add(1);
         testList.add(2);
@@ -21,7 +23,16 @@ public class CollectionsTest {
         assertArrayEquals(new Integer[]{1, 2, 3}, testList.toArray());
     }
 
-    @org.junit.Test
+    @Test
+    public void shellSort1() throws Exception {
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        ru.innopolis.mputilov.Collections.shellSort(testList);
+        assertArrayEquals(new Integer[]{1, 2, 3}, testList.toArray());
+    }
+
+    @Test
     public void insertionSort2() throws Exception {
         testList.add(3);
         testList.add(1);
@@ -30,20 +41,42 @@ public class CollectionsTest {
         assertArrayEquals(new Integer[]{1, 2, 3}, testList.toArray());
     }
 
-    @org.junit.Test
+    @Test
+    public void shellSort2() throws Exception {
+        testList.add(3);
+        testList.add(1);
+        testList.add(2);
+        ru.innopolis.mputilov.Collections.shellSort(testList);
+        assertArrayEquals(new Integer[]{1, 2, 3}, testList.toArray());
+    }
+
+    @Test
     public void insertionSort3() throws Exception {
         ru.innopolis.mputilov.Collections.insertionSort(testList);
         assertArrayEquals(new Integer[]{}, testList.toArray());
     }
 
-    @org.junit.Test
+    @Test
+    public void shellSort3() throws Exception {
+        ru.innopolis.mputilov.Collections.shellSort(testList);
+        assertArrayEquals(new Integer[]{}, testList.toArray());
+    }
+
+    @Test
     public void insertionSort4() throws Exception {
         testList.add(-5);
         ru.innopolis.mputilov.Collections.insertionSort(testList);
         assertArrayEquals(new Integer[]{-5}, testList.toArray());
     }
 
-    @org.junit.Test
+    @Test
+    public void shellSort4() throws Exception {
+        testList.add(-5);
+        ru.innopolis.mputilov.Collections.shellSort(testList);
+        assertArrayEquals(new Integer[]{-5}, testList.toArray());
+    }
+
+    @Test
     public void insertionSort5() throws Exception {
         testList.add(0);
         testList.add(-5);
@@ -57,5 +90,18 @@ public class CollectionsTest {
         assertArrayEquals(new Integer[]{-5, -5, 0, 0, 5, 10, 11}, testList.toArray());
     }
 
+    @Test
+    public void shellSort5() throws Exception {
+        testList.add(0);
+        testList.add(-5);
+        testList.add(0);
+        testList.add(10);
+        testList.add(11);
+        testList.add(5);
+        testList.add(-5);
+        ru.innopolis.mputilov.Collections.shellSort(testList);
+        System.out.println(testList);
+        assertArrayEquals(new Integer[]{-5, -5, 0, 0, 5, 10, 11}, testList.toArray());
+    }
 
 }
